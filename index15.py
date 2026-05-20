ccc7bb451a219357731ed2de007eed97956ed4a9
Aatishprabhu <prabhuaatish09@gmail.com>
2026-05-20 13:02:48 +0530

    update

---
+++ index15.py
@@ -0,0 +1,13 @@
+students = {}
+def add_student(s_id, name, grade):
+    students[s_id] = {"name": name, "grade": grade}
+def update_grade(s_id, new_grade):
+    if s_id in students:
+        students[s_id]["grade"] = new_grade
+    else:
+        print("Student not found.")
+def display_students():
+    for s_id, info in students.items():
+        print(f"ID: {s_id} | Name: {info['name']} | Grade: {info['grade']}")
+add_student(101, "Alice", "A")
+update_grade(101, "A+")
